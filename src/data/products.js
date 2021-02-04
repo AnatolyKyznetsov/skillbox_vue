22b1950576730bb515155ce1e0bf9f8696c60fbf
Anatoly <120422@bk.ru>
2021-02-04 15:28:53 +0700

    задание урока 4

---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Смартфон OPPO Reno4 Lite 8+128GB Magic Blue',
+    price: 24990,
+    img: '/img/phone.jpg',
+  },
+  {
+    title: 'Смарт-часы Samsung Galaxy Watch Active2',
+    price: 24990,
+    img: '/img/watch.jpg',
+  },
+  {
+    title: 'Электробритва Philips',
+    price: 5990,
+    img: '/img/shaver.jpg',
+  },
+  {
+    title: 'Электрическая зубная щетка Braun ORAL-B',
+    price: 2490,
+    img: '/img/toothbrush.jpg',
+  },
+  {
+    title: 'Машинка для стрижки волос Philips',
+    price: 4590,
+    img: '/img/clipper.jpg',
+  },
+  {
+    title: 'Эпилятор Rowenta',
+    price: 4990,
+    img: '/img/epilator.jpg',
+  },
+  {
+    title: 'Пылесос ручной Tefal',
+    price: 11990,
+    img: '/img/vacuum_cleaner.jpg',
+  },
+  {
+    title: 'Утюг Redmond',
+    price: 3990,
+    img: '/img/iron.jpg',
+  },
+  {
+    title: 'Наушники True Wireless Samsung',
+    price: 17990,
+    img: '/img/headphones.jpg',
+  },
+  {
+    title: 'Умная колонка JBL',
+    price: 8790,
+    img: '/img/speaker.jpg',
+  },
+  {
+    title: 'Кофемашина капсульного',
+    price: 7990,
+    img: '/img/coffee_maker.jpg',
+  },
+  {
+    title: 'Радиатор Electrolux',
+    price: 5790,
+    img: '/img/radiator.jpg',
+  },
+];
