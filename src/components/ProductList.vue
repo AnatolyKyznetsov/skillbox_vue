98b351414291a946f9995658fa3bbc2adaa5f5d7
Anatoly <120422@bk.ru>
2021-02-05 19:58:46 +0700

    задание урока 4.4 - id товара и выбор цвета

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,11 +1,11 @@
 <template>
   <ul class="catalog__list">
-    <ProductItem :product="product" v-for="(product, index) in products" :key="index" />
+    <ProductItem :product="product" v-for="product in products" :key="product.id" />
   </ul>
 </template>
 
 <script>
-import ProductItem from './ProductItem';
+import ProductItem from './ProductItem.vue';
 
 export default {
   components: {
--- src/data/products.js
+++ src/data/products.js
@@ -1,60 +1,72 @@
 export default [
   {
+    id: 1,
     title: 'Смартфон OPPO Reno4 Lite 8+128GB Magic Blue',
     price: 24990,
     img: '/img/phone.jpg',
   },
   {
+    id: 2,
     title: 'Смарт-часы Samsung Galaxy Watch Active2',
     price: 24990,
     img: '/img/watch.jpg',
   },
   {
+    id: 3,
     title: 'Электробритва Philips',
     price: 5990,
     img: '/img/shaver.jpg',
   },
   {
+    id: 4,
     title: 'Электрическая зубная щетка Braun ORAL-B',
     price: 2490,
     img: '/img/toothbrush.jpg',
   },
   {
+    id: 5,
     title: 'Машинка для стрижки волос Philips',
     price: 4590,
     img: '/img/clipper.jpg',
   },
   {
+    id: 6,
     title: 'Эпилятор Rowenta',
     price: 4990,
     img: '/img/epilator.jpg',
   },
   {
+    id: 7,
     title: 'Пылесос ручной Tefal',
     price: 11990,
     img: '/img/vacuum_cleaner.jpg',
   },
   {
+    id: 8,
     title: 'Утюг Redmond',
     price: 3990,
     img: '/img/iron.jpg',
   },
   {
+    id: 9,
     title: 'Наушники True Wireless Samsung',
     price: 17990,
     img: '/img/headphones.jpg',
   },
   {
+    id: 10,
     title: 'Умная колонка JBL',
     price: 8790,
     img: '/img/speaker.jpg',
   },
   {
+    id: 11,
     title: 'Кофемашина капсульного',
     price: 7990,
     img: '/img/coffee_maker.jpg',
   },
   {
+    id: 12,
     title: 'Радиатор Electrolux',
     price: 5790,
     img: '/img/radiator.jpg',
