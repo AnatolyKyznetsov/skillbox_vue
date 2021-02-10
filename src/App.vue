9f08b3246693148e9e7a2ad37c1c2f8c6956a9ac
Anatoly <120422@bk.ru>
2021-02-10 11:01:52 +0700

    vuex + новые компоненты

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <router-view />
+  <div>
+    <AppHeader />
+    <router-view />
+    <AppFooter />
+  </div>
 </template>
 
 <script>
+import AppHeader from '@/components/AppHeader.vue';
+import AppFooter from '@/components/AppFooter.vue';
+
 export default {
   name: 'App',
+  components: {
+    AppHeader,
+    AppFooter,
+  },
 };
 </script>
